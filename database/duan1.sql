2522e5051710c578141cbf85d76ac6f891fd3213
dinhtkpc06914 <dinhtkpc06914@gmail.com>
2023-11-16 12:20:30 +0700

    update database

--- database/duan1.sql
+++ database/duan1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 12, 2023 lúc 03:18 AM
+-- Thời gian đã tạo: Th10 16, 2023 lúc 06:18 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.2.4
 
@@ -28,30 +28,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `ban_an` (
-  `id` int(11) NOT NULL COMMENT 'Mã phòng',
-  `ten` varchar(50) NOT NULL COMMENT 'Tên Hàng Hóa',
-  `don_gia` double(10,2) NOT NULL COMMENT 'Đơn Giá',
-  `gia_giam` double(10,2) DEFAULT 0.00 COMMENT 'Giảm Giá',
-  `hinh` varchar(50) NOT NULL COMMENT 'Hình Ảnh',
-  `ngay_nhap` date DEFAULT NULL COMMENT 'Ngày Nhập Hàng',
-  `mo_ta` text NOT NULL COMMENT 'Mô Tả',
-  `dac_biet` tinyint(1) NOT NULL COMMENT 'Đặc Biệt',
-  `luot_xem` int(11) NOT NULL DEFAULT 0 COMMENT 'Số Lượt Xem',
-  `id_loai` int(10) NOT NULL COMMENT 'Mã Loại'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `ma_ban` int(11) NOT NULL,
+  `ma_loai_ban` int(11) DEFAULT NULL,
+  `ten_ban` varchar(255) DEFAULT NULL,
+  `trang_thai` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `ban_an`
 --
 
-INSERT INTO `ban_an` (`id`, `ten`, `don_gia`, `gia_giam`, `hinh`, `ngay_nhap`, `mo_ta`, `dac_biet`, `luot_xem`, `id_loai`) VALUES
-(1, 'Kem sữa dừa', 20000.00, 15000.00, 'product-4.jpg', '2023-10-07', 'Một loại kem mang hương vị của quê hương được làm từ những trái dừa khô và sữa bò .', 1, 80, 1),
-(3, 'Kem chuối', 10000.00, 8000.00, 'kem-chuoi.jpg', '2023-10-07', 'Kem chuối là một món tráng miệng phổ biến ở Việt Nam. Nó được làm từ kem vani và chuối tươ', 0, 16, 2),
-(4, 'Kem sữa chua', 15000.00, 12000.00, 'kem-sua-chua.png', '2023-10-07', 'Loại kem này được làm từ kem và sữa chua. Hương vị của kem sữa chua thường ngọt nhẹ và có chút chua từ sữa chua.', 0, 2, 2),
-(5, 'Kem bơ phủ socola', 10000.00, 1000.00, 'kem-bo.jpg', '2023-10-21', 'Kem bơ phủ socola là một loại kem rất phổ biến và ngon miệng. Để làm kem bơ phủ socola, người ta thường sử dụng bơ và socola làm thành phần chính. Dưới đây là cách làm kem bơ phủ socola đơn giản:', 1, 0, 23),
-(6, 'Kem đậu đỏ', 12000.00, 2000.00, 'kem-dau-do.jpg', '2023-10-21', 'Kem đậu đỏ, hay còn gọi là kem đậu phộng đỏ, là một món tráng miệng phổ biến trong ẩm thực Á Đông, bao gồm cả Việt Nam. Đây là một loại kem thơm ngon và có màu đỏ tự nhiên nhờ sử dụng đậu đỏ làm thành phần chính.', 1, 0, 23),
-(7, 'Half and half', 30000.00, 8000.00, 'kem-tuoi-1.jpg', '2023-10-21', 'Kem Half and Half là một loại kem phổ biến ở Mỹ và một số quốc gia khác. \"Half and Half\" trong tiếng Anh có nghĩa là \"một nửa và một nửa\", và loại kem này thường được làm bằng cách kết hợp một phần sữa và một phần kem.', 0, 0, 25),
-(8, 'Light Whipping cream', 50000.00, 5000.00, 'kem-tuoi-2.jpg', '2023-10-21', 'Kem Light Whipping Cream là một phiên bản kem nhẹ hơn của kem whipping cream truyền thống. Nó có nồng độ chất béo thấp hơn, thường từ 30-35% so với 35-40% của kem whippping cream thông thường. Kem Light Whipping Cream thường được sử dụng để làm kem đánh bông nhẹ hoặc làm phụ gia cho các món tráng miệng khác.', 1, 0, 25);
+INSERT INTO `ban_an` (`ma_ban`, `ma_loai_ban`, `ten_ban`, `trang_thai`) VALUES
+(3, 2, 'cccccccc', 'ccc');
 
 -- --------------------------------------------------------
 
@@ -60,12 +48,62 @@ INSERT INTO `ban_an` (`id`, `ten`, `don_gia`, `gia_giam`, `hinh`, `ngay_nhap`, `
 --
 
 CREATE TABLE `binh_luan` (
-  `id` int(6) NOT NULL,
-  `noi_dung` varchar(255) NOT NULL,
-  `id_ban` int(6) NOT NULL,
-  `id_kh` int(6) NOT NULL,
-  `ngay_bl` date NOT NULL,
-  `xep_hang` int(11) NOT NULL
+  `ma_binh_luan` int(11) NOT NULL,
+  `ma_khach_hang` int(11) DEFAULT NULL,
+  `ma_mon_an` int(11) DEFAULT NULL,
+  `noi_dung` text NOT NULL,
+  `ngay_binh_luan` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `chi_tiet_hoa_don`
+--
+
+CREATE TABLE `chi_tiet_hoa_don` (
+  `ma_chi_tiet` int(11) NOT NULL,
+  `ma_hoa_don` int(11) DEFAULT NULL,
+  `ma_mon_an` int(11) DEFAULT NULL,
+  `so_luong` int(11) DEFAULT NULL,
+  `don_gia` decimal(10,2) DEFAULT NULL,
+  `thanh_tien` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `dat_ban`
+--
+
+CREATE TABLE `dat_ban` (
+  `ma_dat_ban` int(11) NOT NULL,
+  `ten_khachhang` varchar(255) NOT NULL,
+  `so_dien_thoai` varchar(15) NOT NULL,
+  `ngay_dat` date NOT NULL,
+  `gio_dat` time NOT NULL,
+  `so_nguoi` int(11) NOT NULL,
+  `ghi_chu` text DEFAULT NULL,
+  `trang_thai` int(11) DEFAULT 0,
+  `ma_ban` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `dat_mon`
+--
+
+CREATE TABLE `dat_mon` (
+  `ma_dat_mon` int(11) NOT NULL,
+  `ten_khachhang` varchar(255) NOT NULL,
+  `so_dien_thoai` varchar(15) NOT NULL,
+  `ngay_dat` date NOT NULL,
+  `gio_dat` time NOT NULL,
+  `ma_mon_an` int(11) NOT NULL,
+  `so_luong` int(11) NOT NULL,
+  `ghi_chu` text DEFAULT NULL,
+  `trang_thai` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 -- --------------------------------------------------------
@@ -75,25 +113,12 @@ CREATE TABLE `binh_luan` (
 --
 
 CREATE TABLE `hoa_don` (
-  `id` int(6) NOT NULL,
-  `sdt` int(11) NOT NULL,
-  `dia_chi` int(255) NOT NULL,
-  `tong_tien` decimal(10,3) NOT NULL,
-  `id_kh` int(6) NOT NULL,
-  `ngay_dat` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `hoa_don_ct`
---
-
-CREATE TABLE `hoa_don_ct` (
-  `id` int(6) NOT NULL,
-  `id_ban` int(6) NOT NULL,
-  `id_mon` int(6) NOT NULL,
-  `don_gia` decimal(10,3) NOT NULL
+  `ma_hoa_don` int(11) NOT NULL,
+  `ma_ban` int(11) DEFAULT NULL,
+  `ma_khach_hang` int(11) DEFAULT NULL,
+  `ngay_tao` datetime DEFAULT NULL,
+  `tong_tien` decimal(10,2) DEFAULT NULL,
+  `trang_thai` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 -- --------------------------------------------------------
@@ -116,8 +141,9 @@ CREATE TABLE `khach_hang` (
 --
 
 INSERT INTO `khach_hang` (`ma_kh`, `mat_khau`, `ho_ten`, `hinh`, `email`, `sdt`) VALUES
-(1, 'd9b1d7db4cd6e70935368a1efb10e377', 'Trần Khải Đìn', 'mx2.jpg', 'tkd25092003@gmail.com', 363055450),
-(4, '7363a0d0604902af7b70b271a0b96480', 'Quách Nguyễn ', 'mv3.jpg', 'truc@gmail.com', 2147483647);
+(1, '7363a0d0604902af7b70b271a0b96480', 'Trần Khải Đình', 'mx2.jpg', 'tkd25092003@gmail.com', 363055450),
+(4, '1cc39ffd758234422e1f75beadfc5fb2', 'Quách Nguyễn xuân trúc', 'mv3.jpg', 'truc@gmail.com', 2147483647),
+(6, '202cb962ac59075b964b07152d234b70', 'Phan Hữu Phúc', 'mx2.jpg', 'admin@gmail.com.cu', 2147483647);
 
 -- --------------------------------------------------------
 
@@ -126,20 +152,18 @@ INSERT INTO `khach_hang` (`ma_kh`, `mat_khau`, `ho_ten`, `hinh`, `email`, `sdt`)
 --
 
 CREATE TABLE `loai_ban` (
-  `id` int(10) NOT NULL COMMENT 'Mã Loại Hàng',
-  `ten` varchar(50) NOT NULL COMMENT 'Tên Loại '
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `ma_loai_ban` int(11) NOT NULL,
+  `ten_loai_ban` varchar(255) NOT NULL,
+  `mo_ta` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `loai_ban`
 --
 
-INSERT INTO `loai_ban` (`id`, `ten`) VALUES
-(1, 'Kem ký'),
-(2, 'Kem truyền thống'),
-(23, 'Kem cây'),
-(24, 'Kem Sandwich'),
-(25, 'Kem tươi');
+INSERT INTO `loai_ban` (`ma_loai_ban`, `ten_loai_ban`, `mo_ta`) VALUES
+(2, 'Bàn gia đình đẹp trai', '  gia đình mới được đặt nha cưnhg'),
+(4, 'Bàn ngoài trời', 'ngoài trời');
 
 -- --------------------------------------------------------
 
@@ -148,8 +172,8 @@ INSERT INTO `loai_ban` (`id`, `ten`) VALUES
 --
 
 CREATE TABLE `loai_mon` (
-  `id` int(6) NOT NULL COMMENT 'mã loại món ăn',
-  `ten` varchar(100) NOT NULL COMMENT 'tên món ăn'
+  `ma_loai_mon` int(6) NOT NULL COMMENT 'mã loại món ăn',
+  `ten_loai_mon` varchar(100) NOT NULL COMMENT 'tên món ăn'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 -- --------------------------------------------------------
@@ -159,13 +183,14 @@ CREATE TABLE `loai_mon` (
 --
 
 CREATE TABLE `mon_an` (
-  `id` int(6) NOT NULL COMMENT 'mã món ăn',
+  `ma_mon_an` int(6) NOT NULL COMMENT 'mã món ăn',
   `ten` varchar(100) NOT NULL COMMENT 'Tên món ăn',
   `don_gia` varchar(255) NOT NULL COMMENT 'Đơn giá',
   `gia_giam` decimal(10,3) NOT NULL COMMENT 'giá giảm',
   `hinh` varchar(255) NOT NULL COMMENT 'hình ảnh',
   `mo_ta` varchar(255) NOT NULL COMMENT 'mô tả món ăn',
   `dac_biet` tinyint(1) NOT NULL COMMENT 'đặc biệt hay không',
+  `ngay_nhap` date NOT NULL COMMENT 'ngày nhập món',
   `luot_xem` int(6) NOT NULL COMMENT 'lượt xem',
   `id_loai_mon` int(6) NOT NULL COMMENT 'mã loại món'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
@@ -190,7 +215,7 @@ CREATE TABLE `nhan_vien` (
 --
 
 INSERT INTO `nhan_vien` (`ma_nv`, `ho_ten`, `mat_khau`, `hinh`, `email`, `sdt`) VALUES
-(2, 'mai hảo hửi', '', 'mn2.jpg', 'admin@gmail.com.cu', 363055450);
+(2, 'mai hảo hửi đ', '', 'mn2.jpg', 'admin@gmail.com.cu', 363055450);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -200,32 +225,46 @@ INSERT INTO `nhan_vien` (`ma_nv`, `ho_ten`, `mat_khau`, `hinh`, `email`, `sdt`) 
 -- Chỉ mục cho bảng `ban_an`
 --
 ALTER TABLE `ban_an`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `ma_hh` (`id`),
-  ADD KEY `ma_loai` (`id_loai`);
+  ADD PRIMARY KEY (`ma_ban`),
+  ADD KEY `ma_loai_ban` (`ma_loai_ban`);
 
 --
 -- Chỉ mục cho bảng `binh_luan`
 --
 ALTER TABLE `binh_luan`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_bl_ban` (`id_ban`),
-  ADD KEY `id_kh` (`id_kh`);
+  ADD PRIMARY KEY (`ma_binh_luan`),
+  ADD KEY `fk_ma_khach_hang` (`ma_khach_hang`),
+  ADD KEY `fk_ma_mon_an` (`ma_mon_an`);
+
+--
+-- Chỉ mục cho bảng `chi_tiet_hoa_don`
+--
+ALTER TABLE `chi_tiet_hoa_don`
+  ADD PRIMARY KEY (`ma_chi_tiet`),
+  ADD KEY `ma_hoa_don` (`ma_hoa_don`),
+  ADD KEY `ma_mon_an` (`ma_mon_an`);
+
+--
+-- Chỉ mục cho bảng `dat_ban`
+--
+ALTER TABLE `dat_ban`
+  ADD PRIMARY KEY (`ma_dat_ban`),
+  ADD KEY `ma_ban` (`ma_ban`);
+
+--
+-- Chỉ mục cho bảng `dat_mon`
+--
+ALTER TABLE `dat_mon`
+  ADD PRIMARY KEY (`ma_dat_mon`),
+  ADD KEY `ma_mon_an` (`ma_mon_an`);
 
 --
 -- Chỉ mục cho bảng `hoa_don`
 --
 ALTER TABLE `hoa_don`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_hoadon_kh` (`id_kh`);
-
---
--- Chỉ mục cho bảng `hoa_don_ct`
---
-ALTER TABLE `hoa_don_ct`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_hoadonct_ban` (`id_ban`),
-  ADD KEY `id_mon` (`id_mon`);
+  ADD PRIMARY KEY (`ma_hoa_don`),
+  ADD KEY `ma_khach_hang` (`ma_khach_hang`),
+  ADD KEY `ma_ban` (`ma_ban`);
 
 --
 -- Chỉ mục cho bảng `khach_hang`
@@ -238,19 +277,19 @@ ALTER TABLE `khach_hang`
 -- Chỉ mục cho bảng `loai_ban`
 --
 ALTER TABLE `loai_ban`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`ma_loai_ban`);
 
 --
 -- Chỉ mục cho bảng `loai_mon`
 --
 ALTER TABLE `loai_mon`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`ma_loai_mon`);
 
 --
 -- Chỉ mục cho bảng `mon_an`
 --
 ALTER TABLE `mon_an`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`ma_mon_an`),
   ADD KEY `id_loai_mon` (`id_loai_mon`);
 
 --
@@ -267,37 +306,61 @@ ALTER TABLE `nhan_vien`
 -- AUTO_INCREMENT cho bảng `ban_an`
 --
 ALTER TABLE `ban_an`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Mã phòng', AUTO_INCREMENT=9;
+  MODIFY `ma_ban` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT cho bảng `binh_luan`
+--
+ALTER TABLE `binh_luan`
+  MODIFY `ma_binh_luan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `chi_tiet_hoa_don`
+--
+ALTER TABLE `chi_tiet_hoa_don`
+  MODIFY `ma_chi_tiet` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `dat_ban`
+--
+ALTER TABLE `dat_ban`
+  MODIFY `ma_dat_ban` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `dat_mon`
+--
+ALTER TABLE `dat_mon`
+  MODIFY `ma_dat_mon` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `hoa_don`
 --
 ALTER TABLE `hoa_don`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `ma_hoa_don` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `khach_hang`
 --
 ALTER TABLE `khach_hang`
-  MODIFY `ma_kh` int(6) NOT NULL AUTO_INCREMENT COMMENT 'tên đăng nhập', AUTO_INCREMENT=6;
+  MODIFY `ma_kh` int(6) NOT NULL AUTO_INCREMENT COMMENT 'tên đăng nhập', AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT cho bảng `loai_ban`
 --
 ALTER TABLE `loai_ban`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Mã Loại Hàng', AUTO_INCREMENT=26;
+  MODIFY `ma_loai_ban` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `loai_mon`
 --
 ALTER TABLE `loai_mon`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT COMMENT 'mã loại món ăn';
+  MODIFY `ma_loai_mon` int(6) NOT NULL AUTO_INCREMENT COMMENT 'mã loại món ăn';
 
 --
 -- AUTO_INCREMENT cho bảng `mon_an`
 --
 ALTER TABLE `mon_an`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT COMMENT 'mã món ăn';
+  MODIFY `ma_mon_an` int(6) NOT NULL AUTO_INCREMENT COMMENT 'mã món ăn';
 
 --
 -- AUTO_INCREMENT cho bảng `nhan_vien`
@@ -313,33 +376,46 @@ ALTER TABLE `nhan_vien`
 -- Các ràng buộc cho bảng `ban_an`
 --
 ALTER TABLE `ban_an`
-  ADD CONSTRAINT `ban_an_ibfk_1` FOREIGN KEY (`id_loai`) REFERENCES `loai_ban` (`id`);
+  ADD CONSTRAINT `ban_an_ibfk_1` FOREIGN KEY (`ma_loai_ban`) REFERENCES `loai_ban` (`ma_loai_ban`);
 
 --
 -- Các ràng buộc cho bảng `binh_luan`
 --
 ALTER TABLE `binh_luan`
-  ADD CONSTRAINT `binh_luan_ibfk_1` FOREIGN KEY (`id_kh`) REFERENCES `khach_hang` (`ma_kh`),
-  ADD CONSTRAINT `fk_bl_ban` FOREIGN KEY (`id_ban`) REFERENCES `ban_an` (`id`);
+  ADD CONSTRAINT `fk_ma_khach_hang` FOREIGN KEY (`ma_khach_hang`) REFERENCES `khach_hang` (`ma_kh`),
+  ADD CONSTRAINT `fk_ma_mon_an` FOREIGN KEY (`ma_mon_an`) REFERENCES `mon_an` (`ma_mon_an`);
+
+--
+-- Các ràng buộc cho bảng `chi_tiet_hoa_don`
+--
+ALTER TABLE `chi_tiet_hoa_don`
+  ADD CONSTRAINT `chi_tiet_hoa_don_ibfk_1` FOREIGN KEY (`ma_hoa_don`) REFERENCES `hoa_don` (`ma_hoa_don`),
+  ADD CONSTRAINT `chi_tiet_hoa_don_ibfk_2` FOREIGN KEY (`ma_mon_an`) REFERENCES `mon_an` (`ma_mon_an`);
+
+--
+-- Các ràng buộc cho bảng `dat_ban`
+--
+ALTER TABLE `dat_ban`
+  ADD CONSTRAINT `dat_ban_ibfk_1` FOREIGN KEY (`ma_ban`) REFERENCES `ban_an` (`ma_ban`);
+
+--
+-- Các ràng buộc cho bảng `dat_mon`
+--
+ALTER TABLE `dat_mon`
+  ADD CONSTRAINT `dat_mon_ibfk_1` FOREIGN KEY (`ma_mon_an`) REFERENCES `mon_an` (`ma_mon_an`);
 
 --
 -- Các ràng buộc cho bảng `hoa_don`
 --
 ALTER TABLE `hoa_don`
-  ADD CONSTRAINT `fk_hoadon_kh` FOREIGN KEY (`id_kh`) REFERENCES `khach_hang` (`ma_kh`);
-
---
--- Các ràng buộc cho bảng `hoa_don_ct`
---
-ALTER TABLE `hoa_don_ct`
-  ADD CONSTRAINT `fk_hoadonct_ban` FOREIGN KEY (`id_ban`) REFERENCES `ban_an` (`id`),
-  ADD CONSTRAINT `hoa_don_ct_ibfk_1` FOREIGN KEY (`id_mon`) REFERENCES `mon_an` (`id`);
+  ADD CONSTRAINT `hoa_don_ibfk_2` FOREIGN KEY (`ma_khach_hang`) REFERENCES `khach_hang` (`ma_kh`),
+  ADD CONSTRAINT `hoa_don_ibfk_3` FOREIGN KEY (`ma_ban`) REFERENCES `ban_an` (`ma_ban`);
 
 --
 -- Các ràng buộc cho bảng `mon_an`
 --
 ALTER TABLE `mon_an`
-  ADD CONSTRAINT `fk_loai_mon_mon` FOREIGN KEY (`id_loai_mon`) REFERENCES `loai_mon` (`id`);
+  ADD CONSTRAINT `fk_loai_mon_mon` FOREIGN KEY (`id_loai_mon`) REFERENCES `loai_mon` (`ma_loai_mon`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
